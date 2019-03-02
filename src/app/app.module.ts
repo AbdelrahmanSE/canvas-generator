e56a4e90078c91ba1ee4d603c7bab6fea9901917
Abdelrahman ElNaggar <abdelrahmanse@hotmail.com>
2019-03-03 01:51:42 +0200

    Created main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { AppRoutingModule } from './app-routing.module';
+import { HistoryComponent } from './history/history.component';
+import { EditorComponent } from './editor/editor.component';
+import { RectComponent } from './editor/rect/rect.component';
+import { LineComponent } from './editor/line/line.component';
+import { ArcComponent } from './editor/arc/arc.component';
+import { CurveComponent } from './editor/curve/curve.component';
+import { TextComponent } from './editor/text/text.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    EditorComponent,
+    RectComponent,
+    LineComponent,
+    ArcComponent,
+    CurveComponent,
+    TextComponent,
+    CanvasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/editor/rect/rect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  templateUrl: './rect.component.html',
+  styleUrls: ['./rect.component.scss']
+})
+export class RectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
